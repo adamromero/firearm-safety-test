505fc87e1008c56003a42adb2f095acaf5847d10
Adam Romero <adamromero89@gmail.com>
2015-04-03 20:11:50 -0700

    Wrote functions to display when answer is correct or wrong Got continue button to pop up when user checks answer Radio buttons are removed after answering question, so user cannot change answer Continue button proceeds to the next question Fixed styling so elements stay properly in place when resizing browser

--- firearm_safety.js
+++ firearm_safety.js
@@ -1,15 +1,139 @@
+var number = 0;
+var numCorrect = 0;
+
 $(function() {
-	//function loadQuestion() {
-		$("#question1").fadeIn({queue: false, duration: 1500});
-		$("#question1").animate({marginLeft:'+=475px'}, 1000);
-	//}
-	function checkAnswers() {
-		var numCorrect = 0;
-		if (document.frmQuestions.optQuestion1[0].checked) {
-			numCorrect++;
-		} 
-		if (document.frmQuestions.optQuestion2[0].checked) {
-			numCorrect++;
-		} 
-	}
+	loadQuestion();
 });
+
+function loadQuestion() {
+	$("#question" + number).remove();
+	$("span").hide();
+	number++;
+	$("#question" + number).fadeIn({queue: false, duration: 1500});
+	$("#question" + number).animate({marginLeft:'29%'}, 1000);
+	$("#question" + number).css("position", "relative");
+}
+	
+function displayCorrect() {
+	$("span").text("Correct!");
+	$("span").css("color", "#009933");
+}
+
+function displayWrong() {
+	$("span").text("Wrong!");
+	$("span").css("color", "#CC0000");
+}
+
+function checkAnswers() {
+	$("span").show();
+	if (number == 1) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers1").remove();
+		$("#continue1").css("display", "block");
+	} else if (number == 2) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers2").remove();
+		$("#continue2").css("display", "block");
+	} else if (number == 3) {
+		if ($("input:radio[value=false]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers3").remove();
+		$("#continue3").css("display", "block");
+	} else if (number == 4) {
+		if ($("input:radio[value=d]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers4").remove();
+		$("#continue4").css("display", "block");
+	} else if (number == 5) {
+		if ($("input:radio[value=b]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers5").remove();
+		$("#continue5").css("display", "block");
+	} else if (number == 6) {
+		if ($("input:radio[value=b]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers6").remove();
+		$("#continue6").css("display", "block");
+	} else if (number == 7) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers7").remove();
+		$("#continue7").css("display", "block");
+	} else if (number == 8) {
+		if ($("input:radio[value=false]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers8").remove();
+		$("#continue8").css("display", "block");
+	} else if (number == 9) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers9").remove();
+		$("#continue9").css("display", "block");
+	} else if (number == 10) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers10").remove();
+		$("#continue10").css("display", "block");
+	} else if (number == 11) {
+		if ($("input:radio[value=true]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers11").remove();
+		$("#continue11").css("display", "block");
+	} else if (number == 12) {
+		if ($("input:radio[value=false]:checked").val()) {
+			numCorrect++;
+			displayCorrect();
+		} else {
+			displayWrong();
+		}
+		$("#answers12").remove();
+		$("#continue12").css("display", "block");
+	}
+}
+
